1cd517ae37d9d6b1b6607297ef72a6e94fe2c985
Hiren Rana <hbr4@njit.edu>
2020-03-03 20:59:09 -0500

    Added Dockerfile

---
+++ src/CalculatorTestsAll.py
@@ -0,0 +1,70 @@
+import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
+from pprint import pprint
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_subtraction(self):
+        test_data = CsvReader('/src/Subtraction.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+    def test_addition(self):
+        test_data = CsvReader('/src/Addition.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+    def test_multiplication(self):
+        test_data = CsvReader('/src/Multiplication.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+
+    def test_division(self):
+        test_data = CsvReader('/src/Division.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.divide(row['Value 2'], row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+
+    def test_square(self):
+        test_data = CsvReader('/src/Square.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.square(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+
+    def test_sqrt(self):
+        test_data = CsvReader('/src/Square_Root.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.sqrt(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
